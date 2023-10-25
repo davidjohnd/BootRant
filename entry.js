b8ebf54b699a973f37a3f8a0be874c02d924361d
Dave-deJong <dndj81@yahoo.co.uk>
2023-10-25 10:19:46 +0100

    Dev and dependancies added

--- entry.js
+++ entry.js
@@ -14,3 +14,18 @@ const result = await pool.query(queryText);
 return result.rows;
 }
 
+// post entry feature
+export async function createPost(post) {
+    // Query the database to create a post and return the newly created post
+    // Define the SQL query to create the post with mainEntry, date
+    const queryText =
+    "INSERT INTO Journal (mainEntry) VALUES ($1) RETURNING *";
+    // Use the pool object to send the query to the database
+  // passing the mainEntry as a parameter to prevent SQL injection
+  const result = await pool.query(queryText, [
+    Journal.mainEntry
+  ]);
+ // The rows property of the result object contains the created entry
+  // Display entry created
+  return result.rows[0];
+}
