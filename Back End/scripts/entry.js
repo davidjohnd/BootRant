6c8107fc71a7b5c7b739f7254e2084c3a8c62398
Mart510 <martynandrewbillings@gmail.com>
2023-10-25 14:42:44 +0100

    adding front end

---
+++ Back End/scripts/entry.js
@@ -0,0 +1,32 @@
+// Import the pool for the database
+import {pool} from "./index.js";
+
+//Get entry feature
+
+export async function getDate() {
+// fetch entry from the journal table
+const queryText = "SELECT * FROM journal";
+
+// use the pool to send to database
+const result = await pool.query(queryText);
+
+//return results
+return result.rows;
+}
+
+// post entry feature
+export async function createPost(journal) {
+    // Query the database to create a post and return the newly created post
+    // Define the SQL query to create the post with mainEntry, date
+    const queryText =
+    "INSERT INTO Journal (entry_type, post, date ) VALUES ($1,$2, current_date) RETURNING *";
+    // Use the pool object to send the query to the database
+  // passing the mainEntry as a parameter to prevent SQL injection
+  const result = await pool.query(queryText, [
+    journal.entry_type,
+    journal.post,
+  ]);
+ // The rows property of the result object contains the created entry
+  // Display entry created
+  return result.rows[0];
+}
