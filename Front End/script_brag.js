4df6d3842630002411966bc552d7b4fd68662fe4
India Grant <60491066+indiagrant@users.noreply.github.com>
2023-10-26 16:19:42 +0100

    post function working on brag page

--- Front End/script_brag.js
+++ Front End/script_brag.js
@@ -3,7 +3,6 @@
 //creating variable to select the buttons
 const backButton = document.getElementsByClassName("back-button")[0];
 
-
 // create a function to change from index.html to other .htmls
 function changeToHomePage() {
   // Use window.location to navigate to the desired page
@@ -13,7 +12,6 @@ function changeToHomePage() {
 // Event listeners
 backButton.addEventListener("click", changeToHomePage);
 
-
 // make journal interactive to write things in the box to save it to database
 // declare vairable
 // import create post
@@ -21,42 +19,22 @@ backButton.addEventListener("click", changeToHomePage);
 const messageInput = document.getElementById("Message");
 const saveButton = document.getElementById("footerSaveItButton");
 
-saveButton.addEventListener('click', async () => {
+saveButton.addEventListener("click", async () => {
   console.log("clicked");
   // Get message from input field
-  // const message = messageInput.value;
-  const message = "test message"
+  const message = messageInput.value;
+  //const message = "test message"
 
- const postObject = {"entry_type": "brag",
-   "post": "DAVE DAVE DAVE PORK PIES ARE THE BEST"}
+  const postObject = {
+    entry_type: "brag",
+    post: message,
+  };
 
-  await fetch("/journal/", {})
-  console.log("posted! Too easy")
-}) 
-
-  //Check message is empty
-  // if (message.trim() === '') {
-  //   alert('Please enter a message before saving.');
-  // } else {
-  //   // Call createBragPost function to save the message to the database
-  //   try {
-  //     const journal = { post: message };
-  //     const createdBrag = await createBragPost(journal);
-
-  //     // Display success message or handle as needed
-  //     console.log('Brag entry added successfully:', createdBrag);
-  //   } catch (error) {
-  //     // Handle any errors that occur during database insertion
-  //     console.error('Error adding brag entry:', error);
-  //   }
-  // }
-  // // create data object to send as JSON 
-  //   const data = {message};
-
-  // // Send a POST request to your server
-  // createBragPost(message);
-  // clear input field
-//   messageInput.value = '';
-//   }
-
-// )
+  await fetch("/journal/", {
+    method: "POST", // Specify the HTTP method
+    headers: {
+      "Content-Type": "application/json", // Specify that you're sending JSON data
+    },
+    body: JSON.stringify(postObject), // Convert your JSON object to a string and send it
+  });
+});
